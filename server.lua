5303246765a52929bcd5e7c7dac40a9ac941e616
KamuiKody <62488881+KamuiKody@users.noreply.github.com>
2022-11-02 23:58:54 -0400

    fix numbers

--- server.lua
+++ server.lua
@@ -7,20 +7,8 @@ local gangsPath = path:gsub('//', '/')..'/shared/gangs/'
 RegisterNetEvent('k-creator:createItem', function(data)
     local src = source
     if QBCore.Functions.GetIdentifier(src, Config.IdType) == Config.Whitelist then
-        local hunger = 'nil'
-        local thirst = 'nil'
-        local alcohol = 'nil'
         local file = io.open(itemsPath..data['password'], 'a+')
-        if tonumber(data['food']) > 0 then
-            hunger = tonumber(data['food'])
-        end
-        if tonumber(data['water']) > 0 then
-            thirst = tonumber(data['water'])
-        end
-        if tonumber(data['alcohol']) > 0 then
-            alcohol = tonumber(data['alcohol'])
-        end
-        local label = "\n\nImports.Items['"..data.name.."'] = {\r    ['name'] = '"..data.name.."', \r    ['label'] = '"..data.label.."', \r    ['weight'] = '"..data.weight.."', \r    ['type'] = 'item', \r    ['image'] = '"..data.image.."', \r    ['hunger'] = '"..hunger.."', \r    ['thirst'] = '"..thirst.."', \r    ['alcohol'] = '"..alcohol.."', \r    ['unique'] = '"..data['unique'].."', \r    ['useable'] = '"..data['useable'].."', \r    ['shouldClose'] = '"..data['shouldclose'].."', \r    ['combinable'] = nil, \r    ['description'] = '"..data['description'].."' \r}"
+        local label = "\n\nImports.Items['"..data.name.."'] = {\r    ['name'] = '"..data.name.."', \r    ['label'] = '"..data.label.."', \r    ['weight'] = "..data.weight..", \r    ['type'] = 'item', \r    ['image'] = '"..data.image.."', \r    ['hunger'] = "..tonumber(data['food'])..", \r    ['thirst'] = "..tonumber(data['water'])..", \r    ['alcohol'] = "..tonumber(data['alcohol'])..", \r    ['unique'] = "..data['unique']..", \r    ['useable'] = "..data['useable']..", \r    ['shouldClose'] = "..data['shouldclose']..", \r    ['combinable'] = nil, \r    ['description'] = '"..data['description'].."' \r}"
         file:write(label)
         file:close()
         TriggerClientEvent('QBCore:Notify', src, 'Item Created : '..data.label)
@@ -30,6 +18,8 @@ RegisterNetEvent('k-creator:createItem', function(data)
             TriggerEvent('qb-log:server:CreateLog', Config.Log, 'Test Webhook', 'default', label..' added by '..QBCore.Functions.GetIdentifier(src, Config.IdType) )
         end
         if Config.AddNow then
+            --exports['qb-core']:AddItem(data.name,{['name'] = data.name,['label'] = data.label,['weight'] = data.weight,['type'] = 'item',['image'] = data.image,['hunger'] = hunger,['thirst'] = thirst,['alcohol'] = alcohol,['unique'] = data['unique'],['useable'] = data['useable'],['shouldClose'] = data['shouldclose'],['combinable'] = nil,['description'] = data['description']})
+            --TriggerClientEvent('QBCore:Client:UpdateObject', -1)
             TriggerEvent('k-ezjob:ImportUpdate')
             TriggerClientEvent('k-ezjob:Client:ImportUpdate', -1)
         end
